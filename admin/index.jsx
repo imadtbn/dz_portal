80fffbb1062fcea4cc8c53512f50163c5e3b2682
imad tbn <tabaniimadeddine@gmail.com>
2025-08-29 21:18:03 +0200

    Create index.jsx

---
+++ admin/index.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useMemo, useState } from "react";
+
+// ألصق الكود الكامل من المكوّن AdminDashboard هنا
+// الكود موجود بالكامل في الكانفس تحت اسم:
+// "لوحة تحكم لإضافة خدمات جديدة - dz_portal (React صفحة واحدة)"
+
+export default function AdminDashboard() {
+  // ...
+}
