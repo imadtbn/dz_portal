dfde6c104cee968f808fb4035d524b060c685fe1
imad tbn <tabaniimadeddine@gmail.com>
2025-09-11 18:00:05 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,3 @@
-
-
 const CACHE_NAME = "dz-portal-cache-v1";
 const urlsToCache = [
   "/dz_portal/",
@@ -12,32 +10,6 @@ self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-
-
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("dz-cache-v1").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./manifest.webmanifest.json",
-        "./icons/icon-192.png",
-        "./icons/icon-512.png"
-      ]);
     })
   );
 });
