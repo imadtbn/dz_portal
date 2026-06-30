f28edda93e03e549941ce28d2469ce871123d1f0
imad tbn <tabaniimadeddine@gmail.com>
2026-06-30 09:38:45 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,22 @@
-self.addEventListener("install", e => {
-  console.log("Service Worker Installed");
+const CACHE_NAME = "dz_portal-cache-v1";
+
+const urlsToCache = [
+  "/dz_portal/",
+  "/dz_portal/index.html",
+  "/dz_portal/assets/css/style.css",
+  "/dz_portal/assets/icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("activate", e => {
-  console.log("Service Worker Activated");
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
